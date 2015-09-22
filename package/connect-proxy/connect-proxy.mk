9c57771a436e3ff9bf32b418f8afa785f1bfd7b3
xSmurf <xsmurf@users.noreply.github.com>
2015-09-22 23:54:03 +0000

    Added connect-proxy package

---
+++ package/connect-proxy/connect-proxy.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# connect-proxy
+#
+################################################################################
+
+CONNECTPROXY_VERSION = master
+CONNECTPROXY_SITE = https://bitbucket.org/gotoh/connect
+CONNECTPROXY_SITE_METHOD = git
+CONNECTPROXY_LICENSE = GPLv2
+CONNECTPROXY_CFLAGS = $(TARGET_CFLAGS) -I$(@D)
+CONNECTPROXY_CXXFLAGS = $(TARGET_CXXFLAGS) -I$(@D)
+
+define CONNECTPROXY_BUILD_CMDS
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) HOSTCC="$(TARGET_CC)" CXXFLAGS="$(CONNECTPROXY_CXXFLAGS)" CFLAGS="$(CONNECTPROXY_CFLAGS)" -C $(@D)
+endef
+
+define CONNECTPROXY_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/connect $(TARGET_DIR)/usr/bin/connect
+endef
+
+$(eval $(generic-package))
+
