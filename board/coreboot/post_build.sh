77d6bd3fb1aeda4308f400525001b03186b3855b
xSmurf <xsmurf@users.noreply.github.com>
2015-09-23 01:13:59 +0000

    up to 12 tty's

--- board/coreboot/post_build.sh
+++ board/coreboot/post_build.sh
@@ -13,15 +13,13 @@ else
 fi
 
 # Disable dropbear server
-echo "Disabling dropbear server ..."
-chmod a-x $TARGET_DIR/etc/init.d/S50dropbear
+if [ -e $TARGET_DIR/etc/init.d/S50dropbear ]; then
+	echo "Disabling dropbear server ..."
+	chmod a-x $TARGET_DIR/etc/init.d/S50dropbear
+fi
 
 # Enable virtual terminals in inittab
-echo "Enabling virtual terminal 2..8 ..."
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty8::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty7::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty6::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty5::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty4::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty3::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
-sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty2::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
+echo "Enabling virtual terminal 2..12 ..."
+for ii in {12..2}; do
+	sed -E -i 's/GENERIC_SERIAL/GENERIC_SERIAL\ntty'$ii'::askfirst:-\/bin\/login/' "$TARGET_DIR/etc/inittab"
+done
