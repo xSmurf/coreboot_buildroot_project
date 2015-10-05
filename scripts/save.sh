ca5dbd4823e90501c5b3f02d0f84cd1cafb8bbf9
xSmurf <xsmurf@users.noreply.github.com>
2015-10-05 03:59:56 +0000

    Fixed saved script for multiple config files

--- scripts/save.sh
+++ scripts/save.sh
@@ -4,7 +4,13 @@ make busybox-update-config
 make linux-update-config
 make savedefconfig
 
-echo 'BR2_DEFCONFIG="$(BR2_EXTERNAL)/configs/coreboot_defconfig"' >> ../builddir/configs/coreboot_defconfig
 
+for fconf in ../builddir/configs/*_defconfig; do
+	fgrep -q BR2_DEFCONFIG "$fconf"
+	if [ $? -gt 0 ]; then
+		echo 'BR2_DEFCONFIG="$(BR2_EXTERNAL)/configs/'${fconf##*/}'"' >> "$fconf"
+		#echo 'BR2_DEFCONFIG="$(BR2_EXTERNAL)/configs/coreboot_defconfig"' >> ../builddir/configs/coreboot_defconfig
+	fi
+done
 echo 'Saved!'
 echo
