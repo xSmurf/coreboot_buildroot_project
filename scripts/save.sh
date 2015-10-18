1ed7b444bf950338c9fa34e3c06f17e4a6afe50f
xSmurf <xsmurf@users.noreply.github.com>
2015-10-18 22:06:40 +0000

    fixed path in save script

--- scripts/save.sh
+++ scripts/save.sh
@@ -4,12 +4,11 @@ make busybox-update-config
 make linux-update-config
 make savedefconfig
 
-
-for fconf in ../builddir/configs/*_defconfig; do
+BDIR=$(dirname $(dirname ${0}))
+for fconf in $BDIR/configs/*_defconfig; do
 	fgrep -q BR2_DEFCONFIG "$fconf"
 	if [ $? -gt 0 ]; then
 		echo 'BR2_DEFCONFIG="$(BR2_EXTERNAL)/configs/'${fconf##*/}'"' >> "$fconf"
-		#echo 'BR2_DEFCONFIG="$(BR2_EXTERNAL)/configs/coreboot_defconfig"' >> ../builddir/configs/coreboot_defconfig
 	fi
 done
 echo 'Saved!'
